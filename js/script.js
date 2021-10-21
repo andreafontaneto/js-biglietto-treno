fbaabb4eabc26cd57cdcf2fd7e71469decaecb1a
andreafontaneto <andrea.fontaneto@gmail.com>
2021-10-21 15:08:07 +0200

    consegna esercizio + init variabili

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('ciao');
+// calcolare prezzo del biglietto del treno
+
+/*
+
+1- definire variabili/constanti (distanza da percorrere e età del passeggero)
+2- calcolare prezzo biglietto in base ai km da percorrere (€ 0,21/km)
+3- applicare sconto 20% per i minorenni
+4- applicare sconto 40% per gli over 65
+5- dichiarare prezzo finale con i centesimi (X,xx)
+
+*/
+
+let distance = 5 // da sostituire con prompt
+let age = 15 // da sostituire con prompt
+
+console.log(distance);
+console.log(age);
+
